73bbed1355357c32694a6ed6c37558ae4612dda9
Jacob Young <JakeYoung5202@gmail.com>
2019-02-01 10:20:17 -0500

    First commit

---
+++ src/projman1/ProjMan1.java
@@ -0,0 +1,16 @@
+
+package projman1;
+
+public class ProjMan1 {
+
+
+    public static void main(String[] args) 
+    {
+        Young();
+    }
+    public static void Young()
+    {
+        System.out.println("Jacob Young!!");
+    }
+    
+}
